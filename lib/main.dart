23abb3f13847f3b7b30e6ff10813f4668cccc272
Voynere <voynere@yandex.ru>
2020-10-08 00:16:34 +1000

    validate

--- lib/main.dart
+++ lib/main.dart
@@ -6,128 +6,136 @@ class DnsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'DnsApp',
-        theme: ThemeData(
-            primaryColor: Color.fromRGBO(50, 65, 85, 1),
-            textTheme: TextTheme(headline6: TextStyle(color: Colors.red))),
-        home: HomePage(),
-        routes: {
-        '/transfering2': (context) => Transfering2(),
-        },
+      title: 'DnsApp',
+      theme: ThemeData(
+          primaryColor: Colors.orange,
+          primaryTextTheme: TextTheme(
+            headline6: TextStyle(color: Colors.white),
+          ),
+          scaffoldBackgroundColor: Colors.white,
+          accentColor: Colors.white,
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.orange,
+            textTheme: ButtonTextTheme.accent,
+          )),
+      home: HomePage(),
+      routes: {
+        '/summary': (context) => SummaryPage(),
+      },
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
+  final _values = {'firstName': '', 'lastName': '', 'phone': '', 'email': ''};
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-            title: Text('Ввод данных'), leading: Icon(Icons.account_circle)),
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Container(
-              width: 300,
-              child: TextField(
+    return Scaffold(
+      //backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Ввод данных'),
+        //leading: Icon(Icons.account_circle)
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
                   hintText: 'Имя',
                 ),
-                autofocus: false,
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return 'Введите имя';
+                  }
+                  return null;
+                },
               ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
+              SizedBox(height: 16),
+              TextFormField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
                   hintText: 'Фамилия',
                 ),
-                autofocus: false,
               ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
+              SizedBox(height: 16),
+              TextFormField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Email',
+                  hintText: 'e-mail',
                 ),
                 keyboardType: TextInputType.emailAddress,
-                autofocus: false,
               ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
+              SizedBox(height: 16),
+              TextFormField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
                   hintText: 'Телефон',
                 ),
-                autofocus: false,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 35.0),
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/transfering2');
-                },
-                child: Text('Получить ключ',
-                    style: TextStyle(fontSize: 18, color: Colors.white)),
-                color: Color.fromRGBO(205, 65, 85, 1),
+              SizedBox(height: 16),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: RaisedButton(
+                    onPressed: () {
+                      _formKey.currentState.validate();
+                      Navigator.pushNamed(context, '/transfering2');
+                    },
+                    child: Text('Получить ключ'.toUpperCase()),
+                  ),
+                ),
               ),
-            ),
-          ]),
+              SizedBox(height: 32),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class Transfering2 extends StatelessWidget {
+class SummaryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Отправка данных')),
+      appBar: AppBar(title: Text('Отправка данных')),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Ссылка на github',
-                ),
-                autofocus: false,
+          Container(
+            width: 300,
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'ссылка на github',
               ),
+              autofocus: false,
             ),
-            Container(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Ссылка на резюме',
-                ),
-                autofocus: false,
+          ),
+          Container(
+            width: 300,
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'ссылка на резюме',
               ),
+              autofocus: false,
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 35.0),
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/transfering2');
-                },
-                child: Text('Зарегистрироваться',
-                    style: TextStyle(fontSize: 18, color: Colors.white)),
-                color: Color.fromRGBO(205, 65, 85, 1),
-              ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 35.0),
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/summary');
+              },
+              child: Text('Зарегистрироваться',
+                  style: TextStyle(fontSize: 18, color: Colors.white)),
+              color: Color.fromRGBO(205, 65, 85, 1),
             ),
-          ]),
+          ),
+        ]),
       ),
     );
   }
